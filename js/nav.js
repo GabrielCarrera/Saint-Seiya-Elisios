5101903a2ccbb789e0d80c49bdc2ced4c74a6b93
GabrielCarrera <gabriel.carrera@bandtec.com.br>
2020-06-07 21:52:14 -0300

    Novo site - Tarefa de Pesquisa e Inovação (Site Pessoal)

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+function closenav() {
+    document.getElementById("mysidebar").style.width = "0";
+    document.getElementById("box_movement").style.marginLeft = "10%";
+}
+function opennav() {
+    document.getElementById("mysidebar").style.width = "15%";
+    document.getElementById("box_movement").style.marginLeft = "17%";
+}
