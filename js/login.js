30c94ab3b4a261dd00d74809aa959c996f0209fa
GabrielCarrera <gabriel.carrera@bandtec.com.br>
2020-06-10 13:09:10 -0300

    Atualizando e estilizando paginas Games, Autor, Personagens

--- js/login.js
+++
@@ -1,8 +0,0 @@
-function closenav() {
-    document.getElementById("mysidebar").style.width = "0";
-    document.getElementById("login_box_movement").style.marginLeft = "35%";
-}
-function opennav() {
-    document.getElementById("mysidebar").style.width = "15%";
-    document.getElementById("login_box_movement").style.marginLeft = "35%";
-}
--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,16 @@
+// ABERTURA E FECHAMENTO DO MENU LATERAL //
 function closenav() {
     document.getElementById("mysidebar").style.width = "0";
-    document.getElementById("box_movement").style.marginLeft = "10%";
+    document.getElementById("padrao_box_movement").style.marginLeft = "10%";
+    document.getElementById("cadastrar_box_movement").style.marginLeft = "15%";
 }
+
+
 function opennav() {
     document.getElementById("mysidebar").style.width = "15%";
-    document.getElementById("box_movement").style.marginLeft = "17%";
+    document.getElementById("padrao_box_movement").style.marginLeft = "17%";
+    // CONFLITO QUANDO ABRO O CADASTRO, PERGUNTAR SE É POR ESTAR DANDO CONFLITO COM A MESMA TAG
+    document.getElementById("cadastrar_box_movement").style.marginLeft = "25%";
 }
+
+
