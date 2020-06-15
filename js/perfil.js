728daa16887b6aea47d5309ee1b941a3fb6ac0ec
GabrielCarrera <gabriel.carrera@bandtec.com.br>
2020-06-15 01:33:59 -0300

    Ajuste de páginas, conserto da Página Perfil

--- js/perfil.js
+++ js/perfil.js
@@ -9,11 +9,11 @@ function opennav() {
 
     // essas 2 funções irão recuperar seus valores do backend futuramente
     function limiteMaximo() {
-        return 10;
+        return 130;
     }
 
     function limiteMinimo() {
-        return -15;
+        return 0;
     }
 
 	// esse "sortearTemperatura()" será desnecessário quando usarmos o backend futuramente
@@ -41,10 +41,10 @@ function opennav() {
             var posicao = (novoRegistro.leitura + ajusteLimiteMin) / (limiteMaximo()+ajusteLimiteMin) * 100;
 
             // tipo está "baixa" (<25), "ok" (>=25 && <=75) ou "ok" (>75) ?
-            var tipo = (posicao < 25 ? 'baixa' : posicao <=75 ? 'ok' : 'alta');
+            var tipo = (posicao < 40 ? 'fraco' : posicao <=88 ? 'ok' : 'deus');
 
             // trocando a imagem e o estilo do valor da leitura
-            i_status.src = `temp-${tipo}.png`;
+            i_status.src = `./img_common/santo-${tipo}.png`;
 			s_leitura.className = `leitura-${tipo}`;       
             
             // atualizando o valor da leitura na página
@@ -53,7 +53,7 @@ function opennav() {
 		    // função que agenda a recuperação da última leitura para daqui a 3 segundos
 			receberNovasLeituras();	
 
-		}, 3000); // 3000 ms -> 3 segundos
+		}, 5000); // 3000 ms -> 3 segundos
 	}
 
 	// indicando que a função "receberNovasLeituras" será invocada assim que a página carregar
