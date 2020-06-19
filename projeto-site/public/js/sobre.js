19e3b452969246358836e180fbb5990781250834
GabrielCarrera <gabriel.carrera@bandtec.com.br>
2020-06-19 17:19:12 -0300

    inserção pagina sobre

---
+++ projeto-site/public/js/sobre.js
@@ -0,0 +1,12 @@
+// ABERTURA E FECHAMENTO DO MENU LATERAL //
+function closenav() {
+    document.getElementById("mysidebar").style.width = "0";  
+    document.getElementById("sobre_movement").style.marginLeft = "9%";
+}
+
+
+function opennav() {
+    document.getElementById("mysidebar").style.width = "15%";
+    document.getElementById("sobre_movement").style.marginLeft = "17%";
+}
+
